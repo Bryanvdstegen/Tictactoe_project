fc321d72361a5530165de743ddcfe8f955818276
Bryan Tran Lu <btl245@stern.nyu.edu>
2019-07-02 01:15:13 -0400

    Return the board

--- tictactoe_project.py
+++ tictactoe_project.py
@@ -17,3 +17,27 @@ while (running==1):
     for event in pygame.event.get():
         if event.type is QUIT:
             running = 0
+
+def initBoard(ttt):
+
+# initialize the board and return it as a variable
+# ttt : a properly initialized pyGame display variable
+# set up the background surface
+# this function returns the value of the variable background (which is undefined).
+
+    background = pygame.Surface (ttt.get_size())
+    background = background.convert()
+    background.fill ((250,250,250))
+    ## credit to Christian for this 
+
+    # draw grid lines
+
+    # vertical lines
+    pygame.draw.line (background, (0,0,0), (100,0), (100,300), 2) # background = surface variable, (000 = color, black), start and end points
+    pygame.draw.line (background, (0,0,0), (200,0), (200,300), 2)
+
+    # horizontal lines
+    pygame.draw.line (background, (0,0,0), (0,100), (300,100), 2)
+    pygame.draw.line (background, (0,0,0), (0,200), (300,200), 2)
+
+    return background # is all back at this point
