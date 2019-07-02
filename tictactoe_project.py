c9390ac3a5a51152313e0f678ad47e0f33d364dd
Bryan Tran Lu <btl245@stern.nyu.edu>
2019-07-01 23:49:16 -0400

    Game window works

--- tictactoe_project.py
+++ tictactoe_project.py
@@ -8,3 +8,12 @@ from pygame.locals import *
 pygame.init()
 ttt = pygame.display.set_mode((300,325))
 pygame.display.set_caption = ('Tic-Tac-Toe')
+
+
+# Loop until user quits the game
+
+running = 1
+while (running==1):
+    for event in pygame.event.get():
+        if event.type is QUIT:
+            running = 0
