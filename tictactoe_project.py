f5746afcffd65ef990da42dc5a41a5995da1f9cd
Bryan Tran Lu <btl245@stern.nyu.edu>
2019-07-01 23:34:04 -0400

    Set up, import pygame libraries, display

---
+++ tictactoe_project.py
@@ -0,0 +1,10 @@
+# version 2.0 using pygame
+
+# Set up and load pygame libraries
+import pygame
+from pygame.locals import *
+
+# Set display
+pygame.init()
+ttt = pygame.display.set_mode((300,325))
+pygame.display.set_caption = ('Tic-Tac-Toe')
