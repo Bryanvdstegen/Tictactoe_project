32809195258361ad18f2b9fb1c4f94b09fcba328
Bryan Tran Lu <btl245@stern.nyu.edu>
2019-07-02 20:32:44 -0400

    Changed display variable

--- tictactoe_project.py
+++ tictactoe_project.py
@@ -43,53 +43,56 @@ def initBoard(ttt):
                          (((screenWidth) / 3) * (num + 1), (screenHeight - consoleHeight)), 2)
 
     return background # board is all black at this point
-    ## credit to Christian for this 
-(-------------------------------------------------------)
-# Set up and load pygame libraries
-import pygame
-from pygame.locals import *
-
-# Set display
-pygame.init()
-ttt = pygame.display.set_mode((300,325))
-pygame.display.set_caption = ('Tic-Tac-Toe')
-
-# Loop until user quits the game (x out of gameboard)
-
-# create the game board
-board = initBoard (ttt)
-
-running = 1
-while (running==1):
-    for event in pygame.event.get():
-        if event.type is QUIT:
-            running = 0
-
-def initBoard(ttt):
-
-# initialize the board and return it as a variable
-# ttt : a properly initialized pyGame display variable
-# set up the background surface
-# this function returns the value of the variable background (which is undefined).
-
-    background = pygame.Surface (ttt.get_size())
-    background = background.convert()
-    background.fill ((250,250,250))
-    ## credit to Christian for this 
-
-    # draw grid lines
-
-    # draw vertical lines
-    pygame.draw.line (background, (0,0,0), (100,0), (100,300), 2) # background = surface variable, (000 = color, black), start and end points
-    pygame.draw.line (background, (0,0,0), (200,0), (200,300), 2)
-
-    # draw horizontal lines
-    pygame.draw.line (background, (0,0,0), (0,100), (300,100), 2)
-    pygame.draw.line (background, (0,0,0), (0,200), (300,200), 2)
-
-    return background # board is all black at this point
+    ## credit to Christian for this
 
 
+def drawStatus(board):
+    global player, winner
+
+    if player == -1:
+      play = "X"
+    else:
+      play = "O"
+
+    if (winner is None):
+      message = play + "'s turn"
+    else:
+      if(winner == -1):
+        message = "X won!"
+      else:
+        message = "O won!"
+
+    font = pygame.font.Font(None, consoleHeight)
+    text = font.render(message, 1, contentColor)
+
+    board.fill(backgroundColor, (0, screenHeight - consoleHeight, screenWidth, consoleHeight))
+    board.blit(bgimg, (0, screenHeight - consoleHeight))
+    board.blit(text, ((screenWidth - text.get_width()) / 2, screenHeight - consoleHeight))
+
+# Need a variable representing the display,
+# and a variable representing the screen surface
+def showBoard(ttt, board):
+    drawStatus(board)
+    ttt.blit(board, (0, 0))
+    pygame.display.flip()
+
+# (re)draw the game board (board) on the screen
+def clickBoard(board):
+    global grid, player
+
+    (mouseX, mouseY) = pygame.mouse.get_pos()
+    (x, y) = boardPos(mouseX, mouseY)
+
+    if ((grid[x][y] == -1) or (grid[x][y] == 1)):
+        return
+
+    drawMove(board, x, y, player)
+
+    if (player == -1):
+        player = 1
+    else:
+        player = -1
+(-----------------------------------------------)
     # Need a variable representing the display,
     # and a variable representing the screen surface
 def showBoard (ttt, board):
